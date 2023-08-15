5b7d4606df09e46bf553c8674ec2313f84d8bf17
Way2SelfAware <122562668+Way2SelfAware@users.noreply.github.com>
2023-08-14 17:30:46 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+// import the pets array from data.js
+const pets = require("./data");
+
+// init express app
+const express = require("express");
+const app = express();
+
+const PORT = 8080;
+
+//Static Directory
+app.use(express.static("public"));
+
+// GET - / - returns homepage
+app.get("/", (req, res) => {
+  // serve up the public folder as static index.html file
+  res.send("/public/index.html");
+});
+
+// hello world route
+app.get("/api", (req, res) => {
+  res.send("Hello World!");
+});
+
+// get all pets from the database
+app.get("/api/v1/pets", (req, res) => {
+  // send the pets array as a response
+  res.json(pets);
+});
+
+// get pet by owner with query string
+app.get("/api/v1/pets/owner", (req, res) => {
+  // get the owner from the request
+  const owner = req.query.owner;
+  // find the pet in the pets array
+  const pet = pets.find((pet) => pet.owner === owner);
+  // send the pet as a response
+  res.json(pet);
+});
+
+// get pet by name
+app.get("/api/v1/pets/:name", (req, res) => {
+  // get the name from the request
+  const name = req.params.name;
+  // find the pet in the pets array
+  const pet = pets.find((pet) => pet.name === name);
+  if (pet) {
+    res.json(pet);
+  } else {
+    res.status(404).json({ error: "No pets found" });
+  }
+  // send the pet as a response
+});
+
+app.listen(PORT, () => {
+  console.log("Server is listening on port " + PORT);
+});
+
+module.exports = app;
